9812d11188ee37a27eeeed43ec017ba6aa0bd016
Yichen Jiang <pauljyc0929@gmail.com>
2018-02-06 18:29:57 -0500

    finish day3

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -13,21 +13,108 @@ public class Problems {
     }
 
     public static List<Integer> removeKDigits(int[] A, int k) {
-        // TODO: your code here
-        // For now, return a List that's correct size, but contains only 0s
-        List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
-        return l;
+        if (A.length == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        LinkedList<Integer> small = new LinkedList<>();
+        small.addLast(A[0]);
+        int i = 1;
+        while (k>0) {
+            if (i==A.length) {
+                small.removeLast();
+                k--;
+            } else {
+                if (small.size() > 0 && A[i] < small.peekLast()) {
+                    small.removeLast();
+                    k--;
+                } else {
+                    small.addLast(A[i]);
+                    i++;
+                }
+            }
+        }
+        while (i<A.length) {
+            small.addLast(A[i]);
+            i++;
+        }
+        return small;
     }
 
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+        if (n == null) {
+            return true;
+        }
+        Node head = n;
+        Integer len = 1;
+        while (head.next!=null) {
+            len++;
+            head = head.next;
+        }
+        head = n;
+        Integer half = len / 2;
+        Node mid = n;
+        Integer i = 0;
+        while (i<half) {
+            mid = mid.next;
+            i++;
+        }
+        Node prev = mid;
+        Node tail = mid.next;
+        Node next = null;
+        while (tail!=null) {
+            next = tail.next;
+            tail.next = prev;
+            prev = tail;
+            tail = next;
+        }
+        tail = prev;
+        i = 0;
+        while (i<half){
+            if (head.val != tail.val) {
+                return false;
+            }
+            head = head.next;
+            tail = tail.next;
+            i++;
+        }
+        return true;
     }
 
     public static String infixToPostfix(String s) {
-        // TODO
-        return null;
+        Integer i = 0;
+        Stack<Character> operators = new Stack<>();
+        String res = "";
+        Integer para = 0;
+        while (i<s.length()) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            }
+            if (s.charAt(i) == '(') {
+                para++;
+            } else {
+                if (s.charAt(i) >='0' && s.charAt(i) <= '9') {
+                    res = res + s.charAt(i) + ' ';
+                } else {
+                    if (s.charAt(i) == ')') {
+                        res = res + operators.pop() + ' ';
+                        para--;
+                    } else {
+                        if (para>0 || operators.size() == 0) {
+                            operators.push(s.charAt(i));
+                        } else {
+                            res = res + operators.pop() + ' ';
+                            operators.push(s.charAt(i));
+                        }
+
+                    }
+                }
+            }
+            i++;
+        }
+        if (operators.size()!=0) {
+            res = res + operators.pop() + ' ';
+        }
+        return res.substring(0,res.length()-1);
     }
 
 }
