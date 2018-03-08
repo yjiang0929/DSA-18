e7039e6557a7e4f94a35283651293904aea1ee9b
Yichen Jiang <pauljyc0929@gmail.com>
2018-03-08 01:40:38 -0500

    finish tree day1

--- trees/day01/src/AVLTree.java
+++ trees/day01/src/AVLTree.java
@@ -7,11 +7,11 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
     TreeNode<T> delete(TreeNode<T> n, T key) {
         n = super.delete(n, key);
         if (n != null) {
-            // TODO
-            // update the height of the tree using the height of the left and right child
-            // return balance(n)
+            n.height = Math.max(height(n.leftChild), height(n.rightChild)) + 1;
+            return balance(n);
+        } else {
+            return n;
         }
-        return null;
     }
 
     /**
@@ -21,11 +21,11 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
     TreeNode<T> insert(TreeNode<T> n, T key) {
         n = super.insert(n, key);
         if (n != null) {
-            // TODO
-            // update the height of the tree using the height of the left and right child
-            // return balance(n)
+            n.height = Math.max(height(n.leftChild), height(n.rightChild)) + 1;
+            return balance(n);
+        } else {
+            return n;
         }
-        return null;
     }
 
     /**
@@ -43,8 +43,11 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
     // Return the height of the given node. Return -1 if null.
     private int height(TreeNode<T> n) {
-        // TODO
-        return 0;
+        if (n==null) {
+            return -1;
+        } else {
+            return n.height;
+        }
     }
 
     public int height() {
@@ -53,8 +56,19 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
     // Restores the AVL tree property of the subtree. Return the head of the new subtree
     TreeNode<T> balance(TreeNode<T> n) {
-        // TODO: (if you're having trouble, use pseudocode provided in slides)
-        return null;
+        if (balanceFactor(n) >= 2) {
+            if (balanceFactor(n.rightChild) < 0) {
+                n.rightChild = rotateRight(n.rightChild);
+            }
+            return rotateLeft(n);
+        }
+        if (balanceFactor(n) <= -2) {
+            if (balanceFactor(n.leftChild) > 0) {
+                n.leftChild = rotateLeft(n.leftChild);
+            }
+            return rotateRight(n);
+        }
+        return n;
     }
 
     /**
@@ -65,23 +79,32 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
      * most one.
      */
     private int balanceFactor(TreeNode<T> n) {
-        // TODO
-        return 0;
+        return height(n.rightChild)-height(n.leftChild);
     }
 
     /**
      * Perform a right rotation on node `n`. Return the head of the rotated tree.
      */
     private TreeNode<T> rotateRight(TreeNode<T> n) {
-        // TODO
-        return null;
+        TreeNode<T> rotatedNode = n.leftChild;
+        TreeNode<T> rotatedTree = rotatedNode.rightChild;
+        rotatedNode.rightChild = n;
+        n.leftChild = rotatedTree;
+        n.height = 1 + Math.max(height(n.leftChild), height(n.rightChild));
+        rotatedNode.height = 1 + Math.max(height(rotatedNode.leftChild), height(rotatedNode.rightChild));
+        return rotatedNode;
     }
 
     /**
      * Perform a left rotation on node `n`. Return the head of the rotated tree.
      */
     private TreeNode<T> rotateLeft(TreeNode<T> n) {
-        // TODO
-        return null;
+        TreeNode<T> rotatedNode = n.rightChild;
+        TreeNode<T> rotatedTree = rotatedNode.leftChild;
+        rotatedNode.leftChild = n;
+        n.rightChild = rotatedTree;
+        n.height = 1 + Math.max(height(n.leftChild), height(n.rightChild));
+        rotatedNode.height = 1 + Math.max(height(rotatedNode.leftChild), height(rotatedNode.rightChild));
+        return rotatedNode;
     }
 }
--- trees/day01/src/Problems.java
+++ trees/day01/src/Problems.java
@@ -1,7 +1,34 @@
+import java.util.LinkedList;
+
 public class Problems {
 
     public static int leastSum(int[] A) {
-        //TODO
-        return 0;
+        int[] counts = new int[10];
+        for (int digit : A) {
+            counts[digit]++;
+        }
+        int i = 0;
+        for (int j=9; j>=0; j--) {
+            while (counts[j]>0) {
+                A[i] = j;
+                counts[j]--;
+                i++;
+            }
+        }
+        i = 0;
+        int result = 0;
+        int multiplier = 1;
+        boolean isSecond = false;
+        while (i<A.length) {
+            result = result + multiplier * A[i];
+            if (isSecond) {
+                multiplier = multiplier * 10;
+                isSecond = false;
+            } else {
+                isSecond = true;
+            }
+            i++;
+        }
+        return result;
     }
 }
